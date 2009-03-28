8f147d14a4a61e9c9673a9c5cca2c7c91d54b67d
Pouech Eric <eric@localhost.(none)>
2009-03-28 10:05:39 +0100

    Fixed the xpoint test for dwarfO2 flavor

--- wdtp_xpoint.c
+++ wdtp_xpoint.c
@@ -3,32 +3,34 @@
 
 static int xpoint_int = 3;
 
-static int xpoint_write(int* ptr)
+int wdtp_test_xpoint_write(int* ptr)
 {
     *ptr = 3;
     xpoint_int += *ptr;
+    WDTP_INSN_BARRIER();
     return *ptr ^ xpoint_int;
 }
 
-static int xpoint_g(int i)
+int wdtp_test_xpoint_g(int i)
 {
-    int ret;
+    volatile int ret;
     ret = 12 + i;
+    WDTP_INSN_BARRIER();
     return ret;
 }
 
-static int xpoint_cond(void)
+int wdtp_test_xpoint_cond(void)
 {
     int i;
     int ret = 0;
 
     for (i = 0; i < 10; i++)
-        ret += xpoint_g(i);
+        ret += wdtp_test_xpoint_g(i);
     return ret;
 }
 int test_xpoint(int v1, const char** ptr)
 {
-    xpoint_write(&v1);
-    xpoint_cond();
+    wdtp_test_xpoint_write(&v1);
+    wdtp_test_xpoint_cond();
     return 0;
 }
