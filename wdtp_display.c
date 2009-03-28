ab38d72a1efdc0a9f467f3c571e81490d0eea00b
Pouech Eric <eric@localhost.(none)>
2009-03-28 10:05:41 +0100

    Fix the display test for dwarfO2

--- wdtp_display.c
+++ wdtp_display.c
@@ -12,22 +12,22 @@ struct xx
     const char* string;
 };
 
-static int display_bar(const char*, int, int, struct xx);
+static int wdtp_test_display_bar(const char*, int, int, struct xx);
 
-static int display_foo(const char* phase, int a, int b, struct xx xx)
+int wdtp_test_display_foo(const char* phase, int a, int b, struct xx xx)
 {
     struct xx xx2;
-    xx2 = xx;
-    xx2.v -= a * b;
+    WDTP_INSN_BARRIER(); xx2 = xx;
+    WDTP_INSN_BARRIER(); xx2.v -= a * b;
     xx2.string = phase;
-    return display_bar(phase, a, b, xx2);
+    return wdtp_test_display_bar(phase, a, b, xx2);
 }
 
-static int display_bar(const char* phase, int a, int b, struct xx xx)
+int wdtp_test_display_bar(const char* phase, int a, int b, struct xx xx)
 {
     if (a == 0) return 0;
     if (a == 1) return b;
-    return display_foo("left", a - 1, b, xx) + display_foo("right", a - 2, b, xx);
+    return wdtp_test_display_foo("left", a - 1, b, xx) + wdtp_test_display_foo("right", a - 2, b, xx);
 }
 
 int test_display(int argc, const char** ptr)
@@ -37,6 +37,6 @@ int test_display(int argc, const char** ptr)
     xx.v = 34;
     xx.string = "mambo!!";
 
-    v = display_foo("first", 3, 4, xx);
+    v = wdtp_test_display_foo("first", 3, 4, xx);
     return v;
 }
