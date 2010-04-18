45a95808932197860dd14f5f1e3aba69b7e8dbed
Eric Pouech <eric.pouech@orange.fr>
2010-04-18 17:42:28 +0200

    Now that dbghelp prints in backtrace the line of ret-addr - 1 (instead of ret-addr), we can homogenize the results of some tests across various debug formats

--- wdtp_type.c
+++ wdtp_type.c
@@ -11,41 +11,46 @@ struct ms
 };
 
 static int*     foo;
-static int      bar;
+int             wdtp_bar = 0;
 struct ms       first = {0, NULL, NULL};
 struct ms       myarray[] = {{0,NULL, &first},{1,NULL,&myarray[0]},{2,NULL,&myarray[1]}};
 
 static struct ms*       fn(int v, const char* k)
 {
     struct ms* ms = malloc(sizeof(*ms));
-    ms->value = v + *foo * bar;
+    ms->value = v + *foo * wdtp_bar;
     ms->key = k;
     ms->next = first.next;
     first.next = ms;
     return ms;
 }
 
+static struct ms*       fn2(int v, const char* k) {return NULL;}
+
 struct ms* (*wdtp_test_pfn)(int, const char*) = fn;
 
 void test_void(void)
 {
-    bar += *foo;
+    wdtp_bar += *foo;
 }
 
 void test_varargs(int toto, ...)
 {
-    bar *= toto;
+    wdtp_bar *= toto;
 }
 
 int test_type(int argc, const char** ptr)
 {
     struct ms*  ms;
     int         lfoo = 3;
+
+    wdtp_bar += argc;
+    if (argc == 10) wdtp_test_pfn = fn2;
     foo = &lfoo;
     ms = wdtp_test_pfn(12, "foo");
     test_void();
-    test_varargs(12, 3, 4);
+    test_varargs(12, 3, 4, wdtp_bar);
     test_varargs(10, "foo");
 
-    return ms->value;
+    return wdtp_bar + ms->value;
 }
